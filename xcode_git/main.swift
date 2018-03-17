efdac28e1003161a4239925e184a474dbde233df
Min-Chul Song <mcsong@sogang.ac.kr>
2018-03-17 17:30:24 +0900

    Initial Commit

---
+++ xcode_git/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xcode_git
+//
+//  Created by Song on 2018. 3. 17..
+//  Copyright © 2018년 Min-Chul Song. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
